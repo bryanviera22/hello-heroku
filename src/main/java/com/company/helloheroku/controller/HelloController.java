222ade8e734a3399c62f16ae56328a779c29e31f
Bryan Viera <bryanviera08@gmail.com>
2021-11-30 19:01:09 -0500

    first commit

---
+++ src/main/java/com/company/helloheroku/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.company.helloheroku.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/hello", method= RequestMethod.GET);
+}
